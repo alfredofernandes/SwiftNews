124c0d4a50888fcc64b81b791d0d15d05d98101d
Alfredo Fernandes <alfredorfernandes@gmail.com>
2019-07-01 15:40:14 -0400

    Create News on Model folder

---
+++ SwiftNews/SwiftNews/Model/News.swift
@@ -0,0 +1,14 @@
+//
+//  News.swift
+//  SwiftNews
+//
+//  Created by Alfredo Fernandes on 2019-07-01.
+//  Copyright © 2019 Alfredo Fernandes. All rights reserved.
+//
+
+import Foundation
+
+public struct News {
+    private(set) var title: String
+    private(set) var thumbnail: String
+}
