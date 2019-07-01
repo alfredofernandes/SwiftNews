41646e304f39e81db8f3f438923b7e297e23f096
Alfredo Fernandes <alfredorfernandes@gmail.com>
2019-07-01 15:21:57 -0400

    Create ArticleCell on Cell folder

---
+++ SwiftNews/SwiftNews/Views/Cell/ArticleCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleCell.swift
+//  SwiftNews
+//
+//  Created by Alfredo Fernandes on 2019-07-01.
+//  Copyright © 2019 Alfredo Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class ArticleCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
