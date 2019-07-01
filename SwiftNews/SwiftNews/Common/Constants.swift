d02c1ff119cdf952e8d1d09a7c74104144fe2546
Alfredo Fernandes <alfredorfernandes@gmail.com>
2019-07-01 15:12:12 -0400

    Create Constants file on Common folder

---
+++ SwiftNews/SwiftNews/Common/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  SwiftNews
+//
+//  Created by Alfredo Fernandes on 2019-07-01.
+//  Copyright © 2019 Alfredo Fernandes. All rights reserved.
+//
+
+import Foundation
+
+public class Constants {
+    
+}
