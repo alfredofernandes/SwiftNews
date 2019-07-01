24f904e8c9970c3d0f3c7645b118560aa16e5499
Alfredo Fernandes <alfredorfernandes@gmail.com>
2019-07-01 16:03:46 -0400

    Create topImage and descriptionTextView on ArticleDetailVC

--- SwiftNews/SwiftNews/Views/ArticleDetail/ArticleDetailVC.swift
+++ SwiftNews/SwiftNews/Views/ArticleDetail/ArticleDetailVC.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ArticleDetailVC: UIViewController {
+    @IBOutlet weak private var topImage: UIImageView!
+    @IBOutlet weak private var descriptionTextView: UITextView!
+    
+    public var article: News!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = self.article.title
+        
+        self.descriptionTextView.text = self.article.description
     }
 }
