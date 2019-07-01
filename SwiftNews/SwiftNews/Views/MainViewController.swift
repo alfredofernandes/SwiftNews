231b0c5257a2b0d3d39009b407eadd499f139e61
Alfredo Fernandes <alfredorfernandes@gmail.com>
2019-07-01 15:19:56 -0400

    Create tableView on MainViewController

--- SwiftNews/SwiftNews/Views/MainViewController.swift
+++ SwiftNews/SwiftNews/Views/MainViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    @IBOutlet weak private var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
