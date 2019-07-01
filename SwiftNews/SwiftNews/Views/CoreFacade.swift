e91fe05563be889af33a9e369a1f68fe03e60cbb
Alfredo Fernandes <alfredorfernandes@gmail.com>
2019-07-01 17:22:04 -0400

    Create CoreFacade

---
+++ SwiftNews/SwiftNews/Views/CoreFacade.swift
@@ -0,0 +1,12 @@
+//
+//  CoreFacade.swift
+//  SwiftNews
+//
+//  Created by Alfredo Fernandes on 2019-07-01.
+//  Copyright © 2019 Alfredo Fernandes. All rights reserved.
+//
+
+import Foundation
+
+class CoreFacade {
+}
