f3bd30a24b151cf963e950a1a7ffea74b597e1b1
Alfredo Fernandes <alfredorfernandes@gmail.com>
2019-07-01 15:52:14 -0400

    Create ArticleDetailVC

---
+++ SwiftNews/SwiftNews/Views/ArticleDetail/ArticleDetailVC.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleDetailVC.swift
+//  SwiftNews
+//
+//  Created by Alfredo Fernandes on 2019-07-01.
+//  Copyright © 2019 Alfredo Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class ArticleDetailVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
